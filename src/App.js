7e61e040ea5a461dd77b079bea388a51268a425b
Priyanka Mukund <priyanka@kunday.com>
2019-06-24 20:55:51 +1000

    created pet component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-// your code is goin to go here,for now
 // throw new Error("lol");
+const Pet = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Jessi"),
+    React.createElement("h2", {}, "Dog"),
+    React.createElement("h2", {}, "Pomeranian")
+  ]);
+};
+
 const App = () => {
   return React.createElement(
     "div", // what kind of element is that
     { id: "something-important" }, // empty object - all the attributes that you want to give it to the components. // pass into the child element into the div.
-    React.createElement("h1", {}, "Adopt Me!")
+    [
+      React.createElement("h1", {}, "Adopt Me!"),
+      React.createElement(Pet), //if you dint aff {} or adopt me or any other name - it means it doesnot have children.
+      React.createElement(Pet),
+      React.createElement(Pet)
+    ]
   );
 };
 
