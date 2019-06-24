28770ce5070cb6d8c0e77d5fc5f7fce6f88938ed
Priyanka Mukund <priyanka@kunday.com>
2019-06-24 21:06:11 +1000

    using props to display elements.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 // throw new Error("lol");
-const Pet = () => {
+const Pet = props => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Jessi"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "Pomeranian")
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed)
   ]);
 };
 
@@ -13,9 +13,21 @@ const App = () => {
     { id: "something-important" }, // empty object - all the attributes that you want to give it to the components. // pass into the child element into the div.
     [
       React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet), //if you dint aff {} or adopt me or any other name - it means it doesnot have children.
-      React.createElement(Pet),
-      React.createElement(Pet)
+      React.createElement(Pet, {
+        name: "Huskus",
+        animal: "Dog",
+        breed: "Pomeranian"
+      }), //if you dint use {} or adopt me or any other name: "Jessi", animal: "Dog", breed: "Pomeranian" /type of animal/or attributes - it means it doesnot have children.
+      React.createElement(Pet, {
+        name: "Jessi",
+        animal: "Bird",
+        breed: "Parrot"
+      }),
+      React.createElement(Pet, {
+        name: "Peppy",
+        animal: "cat",
+        breed: "Mixed"
+      })
     ]
   );
 };
