5a013fe5bc806fc9086f06d5b50250e77aa2e92e
Priyanka Mukund <priyanka@kunday.com>
2019-06-27 17:50:40 +1000

    add custom babel configuration and details route.

--- src/Details.js
+++ src/Details.js
@@ -1,11 +1,43 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = props => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  state = { loading: true };
 
+  // like useEffect - run once when it is created
+  // used for Ajax request
+  componentDidMount() {
+    // this is immutable - this.prop - coming from the parent.
+    // arrow function does not create new context.
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error);
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading........</h1>;
+    }
+
+    const { animal, breed, location, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adobt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 export default Details;
